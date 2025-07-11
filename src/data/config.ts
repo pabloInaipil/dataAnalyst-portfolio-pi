f70ec0688cce33b8e007124e1f8d6bf764ca0e4c
Pablo Inaipil <pablo.inaipil@gmail.com>
2025-07-11 12:40:16 -0400

    john Doe xd

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,12 @@
 export const config = {
   siteTitle: 'Pablo Inaipil Portfolio',
   description: 'Hi, I am John Doe, a passionate web developer and data analyst. I thrive on solving problems and building innovative solutions with modern technology.',
-  author: 'John Doe',
-  contactEmail: 'johndoe@example.com',
+  author: 'Pablo Inaipil',
+  contactEmail: 'pablo.inaipil@gmail.com',
   keywords: 'portfolio, business intelligence, data analysis, Power BI Reports, Software Engineer, Web Developer',
   favicon: '/favicon.svg',
   social: {
-    linkedin: 'https://www.linkedin.com/in/johndoe/',
-    github: 'https://github.com/johndoe'
+    linkedin: 'https://www.linkedin.com/in/pablo-inaipil/',
+    github: 'https://github.com/pabloInaipil'
   }
 };
